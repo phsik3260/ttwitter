3d83cfabb329bb7a9c5145499f4201861d7f9a9e
parkhyunsik <phsik3260@gmail.com>
2022-06-27 15:32:48 +0900

    Initialize Firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import fb from "./fb";
+
+console.log(fb);
 
 export default function App() {
   return <div>hello world</div>;
---
+++ src/fb.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGv1A5M1r1iFK2M_iJq_MSRW85fUjAQ_4",
+  authDomain: "ttwitter-f7f61.firebaseapp.com",
+  projectId: "ttwitter-f7f61",
+  storageBucket: "ttwitter-f7f61.appspot.com",
+  messagingSenderId: "665404786596",
+  appId: "1:665404786596:web:5c5729e124c204c623e331",
+};
+
+export default initializeApp(firebaseConfig);
