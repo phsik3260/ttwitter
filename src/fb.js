451b1c46d0258a1294fb28d8e72b2224a2d68bbb
parkhyunsik <phsik3260@gmail.com>
2022-06-27 15:49:19 +0900

    Set enviroment variables

--- src/fb.js
+++ src/fb.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGv1A5M1r1iFK2M_iJq_MSRW85fUjAQ_4",
-  authDomain: "ttwitter-f7f61.firebaseapp.com",
-  projectId: "ttwitter-f7f61",
-  storageBucket: "ttwitter-f7f61.appspot.com",
-  messagingSenderId: "665404786596",
-  appId: "1:665404786596:web:5c5729e124c204c623e331",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default initializeApp(firebaseConfig);
